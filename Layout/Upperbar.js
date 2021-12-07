b5c775db3b7558d8bef00aca0bfbe41a679f57f3
Ussf-nassrallah <nassrallah.dev@gmail.com>
2021-12-07 10:57:18 +0100

    add : ProductDetails Page

--- Layout/Upperbar.js
+++ Layout/Upperbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "../styles/Layout/Upperbar.module.scss";
 
-const Upperbar = () => {
-  return <div>Upperbar</div>;
-};
+function Upperbar() {
+  return (
+    <section className={styles.ub}>
+      <p className={styles.ubContent}>
+        Free U.S. Shipping for the holidays ; Free Gift Socks on orders over $50
+      </p>
+    </section>
+  );
+}
 
 export default Upperbar;
---
+++ pages/product/[id]/index.js
@@ -0,0 +1,85 @@
+import React from "react";
+import Image from "next/image";
+
+// icons
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
+// Components
+import AddToCartBtn from "../../../Layout/AddToCartBtn";
+
+// Styles
+import styles from "../../../styles/ProductDetails.module.scss";
+
+function singleProduct({ product }) {
+  let productRating = Math.ceil(product.rating.rate);
+
+  const generateStars = (stars) => {
+    if (stars > 5 || stars < 0) {
+      return ["An error occured !"];
+    }
+    let output = [];
+
+    for (let i = 0; i < stars; i++) {
+      output.push(<AiFillStar className={styles.icon} />);
+    }
+
+    for (let i = stars; i < 5; i++) {
+      output.push(<AiOutlineStar className={styles.icon} />);
+    }
+
+    return output;
+  };
+
+  return (
+    <>
+      <div className={styles.flex}>
+        <Image
+          src={product.image}
+          objectFit="contain"
+          width={500}
+          height={500}
+        />
+
+        <div className={styles.info}>
+          <h2 className={styles.infoTitle}>{product.title}</h2>
+
+          <p className={styles.infoPrice}>$ {product.price}</p>
+
+          {productRating === 5 && <p className={styles.topp}>best seller #1</p>}
+
+          <div className={styles.infoRating}>
+            <div className={styles.infoRatingRate}>
+              {generateStars(productRating)}
+            </div>
+
+            <p className={styles.infoRatingCount}>
+              {"(" + product.rating.count + ")"}
+            </p>
+          </div>
+
+          <span>Details</span>
+
+          <p className={styles.infoDesc}>{product.description}</p>
+
+          <AddToCartBtn />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://fakestoreapi.com/products/${context.params.id}`
+  );
+
+  const product = await res.json();
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+export default singleProduct;
